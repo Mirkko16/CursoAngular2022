5d6470a0d1a4ae852d7a7fa6c78a4691abdcf0b9
Mirkko16 <mirkko1996@hotmail.com>
2022-11-19 18:24:59 -0300

    uso de los comits en mi proyecto

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.css']
 })
-export class CardComponent {
+export class CardComponent implements OnInit {
+
+  public image?:string;
+  public Titulo:string="Angular con interpolacion";
+  @Input() dataEntrante:any;
+  constructor(){ }
+
+  ngOnInit(): void {
+    this.image ="https://cdn.ngamers.net/monthly_2018_12/IMG-20180927-WA0085.jpg.1e0c52d9817394ea427f099b42375fcf.jpg"   
+  }
 
 }
